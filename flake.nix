34289160cc804cd00683ebc545e7bd132e6502bd
Adrián Lattes <adrianlattes@disroot.org>
2022-04-16 19:13:59 +0200

    renombrando carpeta

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         neovimPlugins = lib.mkNeovimPlugins { inherit inputs plugins; pkgs = final; };
         neovimBase = lib.mkNeovim { pkgs = final; config = profiles.base; };
         neovimFull = lib.mkNeovim { pkgs = final; config = profiles.full; };
-        lean-language-server = final.callPackage ./lean-language-server {};
+        lean-language-server = final.callPackage (import ./leanls) {};
       };
     } // utils.lib.eachDefaultSystem (system:
       let
