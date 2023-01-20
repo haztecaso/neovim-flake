15a60f02fe544ad0d5aa9783f62dc9508a0a2f7a
Adrián Lattes <adrianlattes@disroot.org>
2023-01-20 06:01:26 +0100

    vimtex_imaps_leader ç

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -70,6 +70,7 @@ let
         "tex_flavor" = "latex";
         "vimtex_view_method" = "zathura";
         "vimtex_quickfix_mode" = 1;
+        "vimtex_imaps_leader" = "ç";
         "vimtex_quickfix_autoclose_after_keystrokes" = 1;
         "vimtex_quickfix_open_on_warning" = 0;
       };
