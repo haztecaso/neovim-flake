33062c07ca94f7d9ae047e993a1c9efe14914ad4
Adrián Lattes <adrianlattes@disroot.org>
2023-01-20 05:49:30 +0100

    vimtex quickfix autoclose

--- modules/plugins.nix
+++ modules/plugins.nix
@@ -69,7 +69,8 @@ let
       globals = {
         "tex_flavor" = "latex";
         "vimtex_view_method" = "zathura";
-        "vimtex_quickfix_mode" = 1;
+        "vimtex_quickfix_mode" = 2;
+        "vimtex_quickfix_autoclose_after_keystrokes" = 1;
         "vimtex_quickfix_open_on_warning" = 0;
       };
     };
