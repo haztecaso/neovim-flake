422d77443b992b6f4970e11292c460bec41f3204
haztecaso <adrianlattes@disroot.org>
2024-10-02 18:02:18 +0200

    updating flake to use nixvim

--- flake.nix
+++ flake.nix
@@ -1,155 +1,35 @@
 {
-  description = "Neovim configurations for different environments.";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-
-    tidal = { url = "github:mitchmindtree/tidalcycles.nix"; };
-
-    # Plugins
-    chatgpt = { url = "github:jackMort/ChatGPT.nvim"; flake = false; };
-    copilot-lua = { url = "github:zbirenbaum/copilot.lua"; flake = false; };
-    copilot-cmp = { url = "github:zbirenbaum/copilot-cmp"; flake = false; };
-    nvim-neoclip = { url = "github:AckslD/nvim-neoclip.lua"; flake = false; };
-    nvim-which-key = { url = "github:folke/which-key.nvim"; flake = false; };
-    obsidian-nvim = { url = "github:epwalsh/obsidian.nvim"; flake = false; };
-    telescope-repo = { url = "github:cljoly/telescope-repo.nvim"; flake = false; };
-    vim-enuch = { url = "github:tpope/vim-eunuch"; flake = false; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-    let
-      lib = import ./lib.nix;
-      overlay = final: prev: {
-        neovim-nightly = inputs.neovim-nightly-overlay.packages.${final.system}.default;
-        mkNeovim = config: lib.mkNeovim { inherit config; pkgs = final; };
-        mkNeovimNightly = config: lib.mkNeovimNightly { inherit config; pkgs = final; };
-        neovimPlugins = lib.mkPlugins {
-          inherit inputs; plugins = [
-          "chatgpt"
-          "copilot-lua"
-          "copilot-cmp"
-          "nvim-neoclip"
-          "nvim-which-key"
-          "obsidian-nvim"
-          "telescope-repo"
-          "vim-enuch"
-        ];
-          pkgs = final;
-        };
-        neovimDefault = lib.mkNeovim {
-          pkgs = final;
-          config = {
-            completion.enable = true;
-            snippets.enable = true;
-            lsp = {
-              enable = true;
-              languages = {
-                bash = true;
-                clang = true;
-                css = true;
-                docker = true;
-                html = true;
-                json = true;
-                lean = false;
-                lua = true;
-                nix = true;
-                php = true;
-                python = true;
-                tex = true;
-                typescript = true;
-                vimscript = true;
-                yaml = true;
-              };
-            };
-            plugins = {
-              ack = true;
-              commentary = true;
-              copilot = true;
-              enuch = true;
-              fugitive = true;
-              gitgutter = true;
-              gruvbox = true;
-              lastplace = true;
-              nix = true;
-              repeat = true;
-              telescope = true;
-              tidal = false;
-              toggleterm = true;
-              treesitter = true;
-              vim-airline = true;
-              vim-visual-multi = true;
-              vimtex = true;
-              vinegar = true;
-            };
-          };
-        };
-        neovimBase = lib.mkNeovim {
-          pkgs = final;
-          config = {
-            completion.enable = true;
-            snippets.enable = true;
-            lsp.enable = false;
-            plugins = {
-              ack = true;
-              commentary = true;
-              enuch = true;
-              fugitive = true;
-              gitgutter = true;
-              gruvbox = true;
-              lastplace = true;
-              nix = true;
-              repeat = true;
-              telescope = false;
-              tidal = false;
-              toggleterm = true;
-              treesitter = false;
-              vim-airline = true;
-              vim-visual-multi = true;
-              vimtex = true;
-              vinegar = true;
-            };
-          };
-        };
-        lean-language-server = (final.callPackage (import ./pkgs/lean-language-server) { nodejs = final."nodejs-18_x"; }).lean-language-server;
-      };
-    in
-    {
-      inherit overlay;
-      hydraJobs =
+  outputs = { nixvim, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem =
+        { pkgs, system, ... }:
         let
-          system = "x86_64-linux";
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit pkgs;
+            module = import ./config;
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
-          neovimDefault.${system} = pkgs.neovimDefault;
-          neovimBase.${system} = pkgs.neovimBase;
-        };
-    } // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlay inputs.tidal.overlays.default ]; };
-      in
-      rec {
-        packages = {
-          neovimBase = pkgs.neovimBase;
-          neovimDefault = pkgs.neovimDefault;
-          lean-language-server = pkgs.lean-language-server;
-          default = packages.neovimDefault;
-        };
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+          };
 
-        apps.default = {
-          type = "app";
-          program = "${packages.neovimDefault}/bin/nvim";
+          packages = {
+            default = nvim;
+          };
         };
+    };
 
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            # nodePackages.node2nix
-            # packages.lean-language-server
-          ];
-        };
-      });
 }
