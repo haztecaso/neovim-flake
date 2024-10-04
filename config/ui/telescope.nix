ae6e465700ed2fde4a6d250d5c4b3b06a76b0817
haztecaso <adrianlattes@disroot.org>
2024-10-04 10:34:26 +0200

    Telescope styles

--- config/ui/telescope.nix
+++ config/ui/telescope.nix
@@ -16,6 +16,18 @@
         "<C-j>".__raw = "require('telescope.actions').move_selection_next";
         "<C-k>".__raw = "require('telescope.actions').move_selection_previous";
       };
+      layout_config = {
+        horizontal = {
+          width_padding = 0.04;
+          height_padding = 0.1;
+          preview_width = 0.6;
+        };
+        vertical = {
+          width_padding = 0.05;
+          height_padding = 1;
+          preview_height = 0.5;
+        };
+      };
     };
   };
   plugins.web-devicons.enable = true;
