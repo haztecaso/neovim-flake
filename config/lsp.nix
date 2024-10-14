926ebbae4c9be217a01aa2b23012074d70d458dd
haztecaso <adrianlattes@disroot.org>
2024-10-14 11:41:37 +0200

    enabling LSP keymaps and updating config

--- config/lsp.nix
+++ config/lsp.nix
@@ -2,65 +2,65 @@
   plugins = {
     lsp = {
     enable = true;
-        # keymaps = {
-        # silent = true;
-        # lspBuf = {
-        # gd = {
-        #   action = "definition";
-        #   desc = "Goto Definition";
-        # };
-        # gr = {
-        #   action = "references";
-        #   desc = "Goto References";
-        # };
-        # gD = {
-        #   action = "declaration";
-        #   desc = "Goto Declaration";
-        # };
-        # gI = {
-        #   action = "implementation";
-        #   desc = "Goto Implementation";
-        # };
-        # gT = {
-        #   action = "type_definition";
-        #   desc = "Type Definition";
-        # };
-        # K = {
-        #   action = "hover";
-        #   desc = "Hover";
-        # };
-        # "<leader>cw" = {
-        #   action = "workspace_symbol";
-        #   desc = "Workspace Symbol";
-        # };
-        # "<leader>cr" = {
-        #   action = "rename";
-        #   desc = "Rename";
-        # };
-      # "<leader>ca" = {
-        # action = "code_action";
-        # desc = "Code Action";
-      # };
-      # "<C-k>" = {
-        # action = "signature_help";
-        # desc = "Signature Help";
-      # };
-      # };
-      # diagnostic = {
-        # "<leader>cd" = {
-        #   action = "open_float";
-        #   desc = "Line Diagnostics";
-        # };
-        # "[d" = {
-        #   action = "goto_next";
-        #   desc = "Next Diagnostic";
-        # };
-        # "]d" = {
-        #   action = "goto_prev";
-        #   desc = "Previous Diagnostic";
-        # };
-        # };
-      # };
+        keymaps = {
+        silent = true;
+        lspBuf = {
+        gd = {
+          action = "definition";
+          desc = "Goto Definition";
+        };
+        gr = {
+          action = "references";
+          desc = "Goto References";
+        };
+        gD = {
+          action = "declaration";
+          desc = "Goto Declaration";
+        };
+        gI = {
+          action = "implementation";
+          desc = "Goto Implementation";
+        };
+        gT = {
+          action = "type_definition";
+          desc = "Type Definition";
+        };
+        K = {
+          action = "hover";
+          desc = "Hover";
+        };
+        "<leader>cw" = {
+          action = "workspace_symbol";
+          desc = "Workspace Symbol";
+        };
+        "<leader>cr" = {
+          action = "rename";
+          desc = "Rename";
+        };
+      "<leader>ca" = {
+        action = "code_action";
+        desc = "Code Action";
+      };
+      "<C-k>" = {
+        action = "signature_help";
+        desc = "Signature Help";
+      };
+      };
+      diagnostic = {
+        "<leader>cd" = {
+          action = "open_float";
+          desc = "Line Diagnostics";
+        };
+        "[d" = {
+          action = "goto_next";
+          desc = "Next Diagnostic";
+        };
+        "]d" = {
+          action = "goto_prev";
+          desc = "Previous Diagnostic";
+        };
+        };
+      };
     servers = {
       efm.enable = true;
       bashls.enable = true;
@@ -73,7 +73,7 @@
       html.enable = true;
       jsonls.enable = true;
       # leanls.enable = true; #TODO: configure with lean plugin?
-      lua-ls = {
+      lua_ls = {
         enable = true;
         settings = {
           completion.callSnippet = "Both";
@@ -83,12 +83,16 @@
           workspace.library = ["vim.api.nvim_get_runtime_file('', true)"];
         };
       };
-      nil-ls.enable = true;
+      nil_ls.enable = true;
+      nixd.enable = true;
       phpactor.enable = true;
       pyright.enable = true;
-      prolog-ls.enable = true;
+      prolog_ls = {
+        enable = true;
+        package = null; # TODO: find nix prolog lsp package
+      };
       texlab.enable = true;
-      ts-ls.enable = true;
+      ts_ls.enable = true;
       yamlls.enable = true;
     };
   };
