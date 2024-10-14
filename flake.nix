d07555a52c069f4a1926b8fee2d0b2cbabdaf26f
haztecaso <adrianlattes@disroot.org>
2024-10-14 18:30:29 +0200

    adding overlay

--- flake.nix
+++ flake.nix
@@ -9,27 +9,45 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
 
-      perSystem = { pkgs, system, ... }:
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+
+      perSystem = { config, pkgs, system, ... }:
         let
           nixvimLib = nixvim.lib.${system};
-          nvimPackage = imports: nixvim.legacyPackages.${system}.makeNixvimWithModule {
-            inherit pkgs;
-            module = {
-              imports = builtins.map (input: ./config/${input}) imports;
+          nvimPackage = imports:
+            nixvim.legacyPackages.${system}.makeNixvimWithModule {
+              inherit pkgs;
+              module = {
+                imports = builtins.map (input: ./config/${input}) imports;
+              };
             };
-          };
-        in
-        {
+        in {
           packages = {
             default = nvimPackage [ "settings.nix" "core" ];
             core = nvimPackage [ "settings.nix" "core" ];
             full = nvimPackage [ "settings.nix" "core" "full" ];
           };
 
+          overlayAttrs = { nvim = { inherit (config.packages) core full; }; };
+
           checks = {
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [ "settings.nix" "core" "full" ];
-            core = nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [ "settings.nix" "core" ];
-            full = nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [ "settings.nix" "core" "full" ];
+            default =
+              nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [
+                "settings.nix"
+                "core"
+                "full"
+              ];
+            core =
+              nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [
+                "settings.nix"
+                "core"
+              ];
+            full =
+              nixvimLib.check.mkTestDerivationFromNixvimModule nvimPackage [
+                "settings.nix"
+                "core"
+                "full"
+              ];
           };
 
           formatter = pkgs.nixfmt-rfc-style;
