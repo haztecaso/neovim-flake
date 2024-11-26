7740a10a087e0cdf17c338e259a1783d3b031d58
haztecaso <adrianlattes@disroot.org>
2024-11-26 15:21:10 +0100

    metals keybindings

--- config/full/metals.nix
+++ config/full/metals.nix
@@ -3,6 +3,30 @@
   extraConfigLua = ''
     local metals = require ('metals');
     local metals_config = vim.tbl_deep_extend("force", metals.bare_config(), {
+      on_attach = function(client, bufnr)
+        if client.server_capabilities.inlayHintProvider then
+          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
+        require("which-key").add({
+          {
+            "<leader>cm", 
+            function()
+              require("telescope").extensions.metals.commands() 
+            end,
+            desc = "Metals commands", 
+            mode = "n"
+          },
+          {
+            "<leader>cc", 
+            function()
+              require("metals").compile_cascade() 
+            end,
+            desc = "Metals Compile Cascade", 
+            mode = "n"
+          },
+        })
+      end, 
+      init_options = { statusBarProvider = "off", },
       settings = {
         showImplicitArguments = true,
         metalsBinaryPath = "${pkgs.metals}/bin/metals";
